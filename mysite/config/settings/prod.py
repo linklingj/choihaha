4302fdfc22d60a532d3905f6ecee03b189ba955b
Jaehyun Choi <jaehyunc413@gmail.com>
2024-11-29 11:21:32 +0900

    production settings

--- mysite/config/settings/prod.py
+++ mysite/config/settings/prod.py
@@ -2,8 +2,9 @@ from .base import *
 
 ALLOWED_HOSTS = ['43.201.203.109', 'choihaha.com']
 
+#  수정할 것
 DEBUG = False
 
-SECURE_SSL_REDIRECT = True
+
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
