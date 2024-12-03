9275934d63b7d20e180af434595c670b4d4d53a0
Jaehyun Choi <jaehyunc413@gmail.com>
2024-12-03 16:15:19 +0900

    config

--- mysite/config/settings/base.py
+++ mysite/config/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=$)3wl6&nf6y@yxm07g*+6bl3py%eq%owi1^yt2*$45-s-97ew'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- mysite/config/settings/prod.py
+++ mysite/config/settings/prod.py
@@ -2,10 +2,4 @@ from .base import *
 
 ALLOWED_HOSTS = ['43.201.203.109', 'choihaha.com']
 
-STATIC_URL = BASE_DIR / 'static/'
-STATICFILES_DIRS = []
-
 DEBUG = False
-
-# change
-SECURE_SSL_REDIRECT = False
